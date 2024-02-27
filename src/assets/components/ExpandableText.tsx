4af80f17066478f36bd4642fc90d3a7f14667c42
Bappy <http.bappy@gmail.com>
2024-02-28 01:35:22 +0600

    Added expandable text component

---
+++ src/assets/components/ExpandableText.tsx
@@ -0,0 +1,33 @@
+import {useState} from "react";
+import Button from "./Button";
+
+interface props {
+    children: string;
+    maxChars?: number;
+
+}
+
+const ExpandableText = ({children, maxChars = 100}: props) => {
+    let [textLength, setMaxChars] = useState(maxChars);
+    let [buttonText, setButtonText] = useState("Show More");
+
+    let handleButtonClick = () => {
+        let length = textLength === children.length ? maxChars : children.length;
+        let text = textLength === children.length ? "Show More" : "Show Less";
+
+        setButtonText(text);
+        setMaxChars(length);
+    };
+
+    return (
+        <>
+            <div>
+                {children.slice(0, textLength)} {textLength < children.length && '...'}
+                <Button onClick={handleButtonClick} text={buttonText} color={'secondary'}/>
+            </div>
+
+        </>
+    );
+};
+
+export default ExpandableText;
