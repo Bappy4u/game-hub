9a79fe971f1476c2976970df4b2dc9f87504782b
Bappy <http.bappy@gmail.com>
2024-02-28 01:35:41 +0600

    Added in expandable text in main app

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Button from "./assets/components/Button";
 import Alert from "./assets/components/Alert";
 import Like from "./assets/components/Like/Like"
 import {useState} from "react";
+import ExpandableText from "./assets/components/ExpandableText";
 
 
 function App() {
@@ -37,6 +38,13 @@ function App() {
             <Button text="Primary Button" color="primary" onClick={handleButtonClick}></Button>
             <Button text="Secondary Button" color="secondary" onClick={handleButtonClick}></Button>
             <Button text="Danger Button" color="danger" onClick={handleButtonClick}></Button>
+
+            <ExpandableText maxChars={50}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+                been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of
+                type and scrambled it to make a type specimen book. It has survived not only five centuries, but also
+                the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
+                with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+                publishing software like Aldus PageMaker including versions of Lorem Ipsum.</ExpandableText>
         </div>);
 }
 
